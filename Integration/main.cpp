b7148c81ee428bf59ce2eac8a64df513a54968a4
Konstantin Mishukov <kkmishukov@gmail.com>
2019-06-12 19:24:41 +0300

    Initial Commit

---
+++ Integration/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Integration
+//
+//  Created by Konstantin Mishukov on 12/06/2019.
+//  Copyright © 2019 Konstantin Mishukov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
